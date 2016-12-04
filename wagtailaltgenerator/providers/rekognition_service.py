dcaa80587b70144dd7f05c48c8c82e29a7a1d1c5
Rob Moorman <rob@moori.nl>
2016-12-04 16:28:17 +0100

    Added AWS RekognitionService service Fix creating module from string class

---
+++ wagtailaltgenerator/providers/rekognition_service.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import boto3
+import requests
+from django.conf import settings
+from wagtailaltgenerator.providers import AbstractProvider, DescriptionResult
+
+
+class RekognitionService(AbstractProvider):
+
+    def __init__(self, *args, **kwargs):
+        self.client = boto3.client('rekognition')
+        super(RekognitionService, self).__init__(*args, **kwargs)
+
+    def describe(self, image):
+        image_url = image.file.url
+        image_data = requests.get(image_url)
+
+        description = None
+        tags = []
+
+        if image_data.status_code == 200:
+            response = self.client.detect_labels(
+                Image={
+                    'Bytes': image_data.content,
+                },
+                MinConfidence=50
+            )
+            if response['Labels']:
+                for label in response['Labels']:
+                    tags.append(label['Name'])
+
+        return DescriptionResult(
+            description=description,
+            tags=tags,
+        )
