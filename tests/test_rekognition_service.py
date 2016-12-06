99635a15bc63d575c892c6d2b1d0cf4a71f682fc
Martin Sandström <martin@marteinn.se>
2016-12-06 23:04:22 +0100

    Created test case for rekognition service

---
+++ tests/test_rekognition_service.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+
+from wagtail.wagtailimages.models import Image
+
+from wagtailaltgenerator.providers import get_provider
+from wagtailaltgenerator.providers.rekognition_service import RekognitionService
+from tests.factories import MockedUrlImageFile, ImageFactory
+
+
+test_image = 'https://oxfordportal.blob.core.windows.net/vision/Analysis/3.jpg'
+
+
+class RekognitionTagTest(TestCase):
+    def test_provider_describe(self):
+        image = ImageFactory()
+        image.file = MockedUrlImageFile(image_url=test_image)
+
+        provider = get_provider(
+            'wagtailaltgenerator.providers.rekognition_service.RekognitionService'
+        )
+
+        data = provider().describe(image)
+
+        self.assertIsNone(data.description)
+        self.assertTrue(len(data.tags) > 0)
