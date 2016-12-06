6b68eeebaf06e40df97be6160da245e9e4810e62
Martin Sandström <martin@marteinn.se>
2016-12-06 22:38:50 +0100

    Added test factories

---
+++ tests/factories.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import, unicode_literals
+
+from django.core.files.images import ImageFile
+import factory
+from wagtail.wagtailimages.tests.utils import get_test_image_file
+from wagtail.wagtailimages.models import Image
+
+
+class ImageFactory(factory.DjangoModelFactory):
+    title = factory.sequence(lambda x: 'image-{0}'.format([x]))
+    file = factory.LazyAttribute(lambda x: get_test_image_file())
+
+    class Meta:
+        model = Image
+
+
+class MockedUrlImageFile(object):
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        self.image_url = kwargs.pop('image_url')
+
+    @property
+    def url(self):
+        return self.image_url
