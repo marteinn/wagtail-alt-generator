cb6e2be1d6b958ed2cfc179ddab2ad3cec855d90
Martin Sandström <martin@marteinn.se>
2019-04-16 16:29:30 +0200

    Refactor signals and add tests

--- wagtailaltgenerator/signals.py
+++ wagtailaltgenerator/signals.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import, unicode_literals
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.conf import settings
 from wagtail.images import get_image_model
 
 from wagtailaltgenerator.providers import get_current_provider
@@ -16,8 +17,13 @@ from wagtailaltgenerator.app_settings import get_setting
 image_cls = get_image_model()
 
 
-@receiver(post_save, sender=image_cls, dispatch_uid="apply_image_alt")
+@receiver(
+    post_save,
+    sender=image_cls,
+    dispatch_uid="wagtailaltgenerator.signals.apply_image_alt",
+)
 def apply_image_alt(sender, instance, **kwargs):
+    # Only run on new images
     if not kwargs['created']:
         return
 
@@ -30,26 +36,24 @@ def apply_image_alt(sender, instance, **kwargs):
         result = translate_description_result(result)
 
     if image_url[-4:] == instance.title[-4:]:
-        _apply_title(instance, result)
+        _apply_title(instance, result.description)
 
-    if app_settings.ALT_GENERATOR_USE_TAGS:
-        _apply_tags(instance, result)
+    if get_setting("ALT_GENERATOR_USE_TAGS"):
+        _apply_tags(instance, result.tags)
 
     instance.save()
 
 
-def _apply_title(instance, result):
-    if not result.description:
+def _apply_title(instance, description):
+    if not description:
         return
 
-    instance.title = result.description
+    instance.title = description
 
 
-def _apply_tags(instance, result):
-    if not result.tags:
+def _apply_tags(instance, tags):
+    if not tags:
         return
-
-    tags = result.tags
 
     if app_settings.ALT_GENERATOR_MAX_TAGS != -1:
         tags = tags[:app_settings.ALT_GENERATOR_MAX_TAGS]
