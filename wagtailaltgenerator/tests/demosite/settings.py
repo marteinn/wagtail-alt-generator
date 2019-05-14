4ad828dc71c4663f101e48ad9068a78936e0313f
Martin Sandström <martin@marteinn.se>
2019-05-14 21:54:45 +0200

    Add missing template middleware required for test

--- wagtailaltgenerator/tests/demosite/settings.py
+++ wagtailaltgenerator/tests/demosite/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
                 'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ]
         }
     }
